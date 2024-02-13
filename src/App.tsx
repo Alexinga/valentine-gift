506dc8a6c04039b4b3f6a5626d4b6f01ef7f1e31
Alex Inga <alexinga98@gmail.com>
2024-02-13 09:25:42 -0500

    added vanta js

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 import { useState } from "react";
-// type AppProps = {
-//   isTrue: boolean;
-// };
+import { useEffect, useRef } from "react";
+//@ts-expect-error Description for ts expect error
+import BIRDS from "vanta/src/vanta.birds";
 
 function App() {
+  useEffect(() => {
+    BIRDS({
+      el: "#vanta",
+      mouseControls: true,
+      touchControls: true,
+      gyroControls: false,
+      minHeight: 200.0,
+      minWidth: 200.0,
+      scale: 1.0,
+      scaleMobile: 1.0,
+      backgroundColor: 0x2f071b,
+    });
+  }, []);
+
   const [isTrue, setIsTrue] = useState(false);
   const [num, setNum] = useState(1);
+  const sectionRef = useRef<HTMLDivElement>(null);
   function showTrue() {
     setIsTrue(!isTrue);
   }
@@ -18,54 +33,60 @@ function App() {
   }
   const numToString = num.toString();
   return (
-    <section>
-      <div className="flex h-screen items-center justify-center flex-col gap-4">
-        {isTrue ? (
-          <iframe
-            src="https://giphy.com/embed/MDJ9IbxxvDUQM"
-            width="280"
-            height="270"
-            allowFullScreen
-          ></iframe>
-        ) : (
-          <iframe
-            src="https://giphy.com/embed/ffzhLUixCtlsc"
-            width="290"
-            height="280"
-            allowFullScreen
-          ></iframe>
-        )}
-        <h1 className="text-white text-sm text-center">
-          {isTrue
-            ? "You mean the whole world to me my meatball! HAPPY VALENTINES DAY!"
-            : "Bella will you be my valentine?"}
-        </h1>
-        {isTrue ? (
-          <button
-            onClick={handleReset}
-            className="text-white text-sm border-2 px-4 py-2 rounded-md"
-          >
-            Reset
-          </button>
-        ) : (
-          <div className="space-x-8">
+    <div
+      id="vanta"
+      className="bg-gradient-to-t from-pink-400 to-red-200"
+      ref={sectionRef}
+    >
+      <section>
+        <div className="flex h-screen items-center justify-center flex-col gap-4">
+          {isTrue ? (
+            <iframe
+              src="https://giphy.com/embed/MDJ9IbxxvDUQM"
+              width="180"
+              height="170"
+              allowFullScreen
+            ></iframe>
+          ) : (
+            <iframe
+              src="https://giphy.com/embed/ffzhLUixCtlsc"
+              width="190"
+              height="180"
+              allowFullScreen
+            ></iframe>
+          )}
+          <h1 className="text-white text-sm text-center">
+            {isTrue
+              ? "You mean the whole world to me my Bella! HAPPY VALENTINES DAY!"
+              : "Bella will you be my valentine?"}
+          </h1>
+          {isTrue ? (
             <button
-              onClick={showTrue}
-              className="text-white border-2 px-4 py-2 rounded-md"
-              style={{ fontSize: `${numToString}rem` }}
-            >
-              Yes
-            </button>
-            <button
-              onClick={handleSizeChange}
+              onClick={handleReset}
               className="text-white text-sm border-2 px-4 py-2 rounded-md"
             >
-              No
+              Reset
             </button>
-          </div>
-        )}
-      </div>
-    </section>
+          ) : (
+            <div className="space-x-8">
+              <button
+                onClick={showTrue}
+                className="text-white border-2 px-4 py-2 rounded-md"
+                style={{ fontSize: `${numToString}rem` }}
+              >
+                Yes
+              </button>
+              <button
+                onClick={handleSizeChange}
+                className="text-white text-sm border-2 px-4 py-2 rounded-md"
+              >
+                No
+              </button>
+            </div>
+          )}
+        </div>
+      </section>
+    </div>
   );
 }
 
